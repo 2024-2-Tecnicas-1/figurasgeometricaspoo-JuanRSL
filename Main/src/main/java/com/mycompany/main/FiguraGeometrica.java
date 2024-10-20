2d3378a364d41ae96342fa85cfc4dcb9201ca772
Juan <jersrodriguez0@gmail.com>
2024-10-20 13:04:16 -0500

    Taller Terminado
    Se realizó la clase padre y todas sus clases derivadas, con sus respectivos atributo y metodos

---
+++ Main/src/main/java/com/mycompany/main/FiguraGeometrica.java
@@ -0,0 +1,32 @@
+package com.mycompany.main;
+
+public abstract class FiguraGeometrica {
+
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract double obtenerArea();
+
+    public abstract double obtenerPerimetro();
+}
